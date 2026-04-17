765c112490ffa9b0a43a076adf736fe93f5fb99c
Eduardo Monteiro de Andrade <edu22snt@gmail.com>
2026-04-17 13:26:53 -0300

    configuração do router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/main-layout/main-layout.component')
+        .then(m => m.MainLayoutComponent),
+    children: [
+      {
+        path: 'principal',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard.component')
+            .then(m => m.DashboardComponent)
+      },
+      {
+        path: 'prestacao-servico',
+        loadComponent: () =>
+          import('./pages/prestacao-servico/prestacao-servico.component')
+            .then(m => m.PrestacaoServicoComponent)
+      },
+      {
+        path: 'repasse-bancorbras',
+        loadComponent: () =>
+          import('./pages/repasse-bancorbras/repasse-bancorbras.component')
+            .then(m => m.RepasseBancorbrasComponent)
+      },
+      {
+        path: 'repasse-hs',
+        loadComponent: () =>
+          import('./pages/repasse-hs/repasse-hs.component')
+            .then(m => m.RepasseHsComponent)
+      },
+      {
+        path: 'usuario',
+        loadComponent: () =>
+          import('./pages/usuario/usuario.component')
+            .then(m => m.UsuarioComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'principal',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
